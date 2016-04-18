7dc9c8a2eb99ad14c63db64487396579a1fe3946
vikashiscool <vikash_parekh@yahoo.com>
2016-04-18 16:15:10 -0700

    Dependency boilerplate (index.js). Create config files.

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
 var express = require('express'),
     app = express(),
+    port = process.env.PORT || 8080,
+    mongoose = require('mongoose'),
+    passport = require('passport'),
+    flash = require('connect-flash'),
+    session = require('express-session'),
     bodyParser = require('body-parser'),
     cookieParser = require('cookie-parser'),
-    mongoose = require('mongoose'),
-    passport = require('passport');
+    configDB = require('./config/database.js');
+
+// require('./config/passport')(passport); // pass passport for configuration
+
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
+
+// mongoose.connect('mongodb://localhost/moneta'); // connect to our database
 
 
+app.use(cookieParser()); // read cookies (needed for auth)
 
-// configure bodyParser (for receiving form data)
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true })); // configure bodyParser (for receiving form data)
 
 // serve static files from public folder
 app.use(express.static(__dirname + '/public'));
+
+// required for passport
+app.use(session({ secret: 'moneymoneymoney' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+
 
 // set view engine to hbs (handlebars)
 // app.set('view engine', 'hbs');
 
 // connect to mongodb
-mongoose.connect('mongodb://localhost/mean_sample');
+// mongoose.connect('mongodb://localhost/moneta');
+
+
 
 
 app.get('/', function (req, res) {
