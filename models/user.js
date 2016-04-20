914fa77e9fdb2be9edfe6e68b2aa5e34504c1c05
vikashiscool <vikash_parekh@yahoo.com>
2016-04-20 12:24:01 -0700

    Update user model with social media, and create methods for generating a hash and checking if password is valid

--- models/user.js
+++ models/user.js
@@ -1,17 +1,41 @@
 var mongoose = require("mongoose");
+var bcrypt = require('bcrypt-nodejs');
 
 var Schema = mongoose.Schema;
 
-var userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true
-  },
+var userSchema = new Schema({
+    local: {
+      email: String,
+      password: String,
+    },
+    facebook: {
+        id: String,
+        token: String,
+        email: String,
+        name: String
+    },
+    twitter: {
+        id: String,
+        token: String,
+        displayName: String,
+        username: String
+    },
+    google: {
+        id: String,
+        token: String,
+        email: String,
+        name: String
+    },
+
+  // name: {
+  //   type: String,
+  //   required: true
+  // },
+  // email: {
+  //   type: String,
+  //   unique: true,
+  //   required: true
+  // },
   // hash: String,
   // salt: String,
   //RESEARCH PASSWORD INCLUSION IN MODEL -- AUTHENTICATION
@@ -31,10 +55,22 @@ var userSchema = new mongoose.Schema({
   admin: {
     type: Boolean,
     required: true
+  },
   todo: [TodoSchema]  
-  }
 });
 
-var User = mongoose.model("User", UserSchema);
+// methods ======================
+// generating a hash
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
 
+// checking if password is valid
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+
+//create the model for users and expose it to our app
+var User = mongoose.model("User", UserSchema);
 module.exports = User;
