89e4a4c9dd3e368feed77b1539e24b88c780c3ec
vikashiscool <vikash_parekh@yahoo.com>
2016-04-21 14:19:17 -0700

    FB strategy (passportjs) and FB routes:

--- config/passport.js
+++ config/passport.js
@@ -2,9 +2,15 @@
 
 // load all the things we need
 var LocalStrategy = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
+
 
 // load the user model
 var User = require('../models/user');
+
+//load auth variables
+var configAuth = require('./auth');
+
 
 // expose this function to the rest of our app using module.exports
 module.exports = function(passport) {
@@ -117,7 +123,66 @@ module.exports = function(passport) {
     }));
 
 
-};
+    // =========================================================================
+    // FACEBOOK ================================================================
+    // =========================================================================
+    passport.use(new FacebookStrategy({
+
+        // pull in our app id and secret from our auth.js file
+        clientID        : configAuth.facebookAuth.clientID,
+        clientSecret    : configAuth.facebookAuth.clientSecret,
+        callbackURL     : configAuth.facebookAuth.callbackURL
+
+    },
+    // The callback passes back user profile info, and each service (Facebook, Twitter, and Google) will pass it back a different way.
+    // Passport standardizes info that comes back in its profile object. 
+    // (So passport object is passed from serverjs to passportjs.) Go Passport!
+
+    // facebook will send back the token and profile
+    function(token, refreshToken, profile, done) {
+
+        // asynchronous
+        process.nextTick(function() {
+
+            // find the user in the database based on their facebook id
+            User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+
+                // If there is an error, stop everything and return that error...
+                // For example, an error connecting to the database
+                if (err)
+                    return done(err);
+
+                // If the user is found, then log them in
+                if (user) {
+                    return done(null, user); // user found, return that user
+                } else {
+                    // If there is no user found with that facebook id, create them
+                    var newUser            = new User();
+
+                    // Set all of the facebook information in our user model
+                    newUser.facebook.id    = profile.id; // Set the user's facebook id                   
+                    newUser.facebook.token = token; // Save the token that facebook provides to the user                    
+                    newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
+                    newUser.facebook.email = profile.emails[0].value; // Facebook can return multiple emails, so we'll take the first
+
+                    // Save our user to the database
+                    newUser.save(function(err) {
+                        if (err)
+                            throw err;
+
+                        // if successful, return the new user
+                        return done(null, newUser);
+                    });
+                }
+
+            });
+        });
+
+    }));
+
+}; 
+
+
 
 
 
--- routes.js
+++ routes.js
@@ -48,11 +48,26 @@ module.exports = function(app, passport) {
     // =====================================
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
+    //route for showing profile page
     app.get('/profile', isLoggedIn, function(req, res) {
         res.sendFile(__dirname + '/views/profile.html', {
             user : req.user // get the user out of session and pass to template
         });
     });
+
+    // =====================================
+    // FACEBOOK ROUTES =====================
+    // =====================================
+    // route for facebook authentication and login
+    app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+    // handle the callback after facebook has authenticated the user
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', {
+            successRedirect : '/profile',
+            failureRedirect : '/'
+        }));
+
 
     // =====================================
     // LOGOUT ==============================
