3e34843ca78778f32150091c7b9497d1440eb3a3
mseegan <mseegan@gmail.com>
2016-04-26 19:06:20 -0700

    fixed toggling of login/signup to actually toggle between them

--- public/scripts/loginapp.js
+++ public/scripts/loginapp.js
@@ -3,14 +3,25 @@ var app = angular.module('login', []);
   
 	app.controller("loginController", ['$scope', function($scope) {
 		$scope.sign = "Log In";
-		
+		$scope.account = "Need an Account?";
+		$scope.login = true;
 
 		$scope.signup = function(){
-			$scope.sign = "Sign Up";
-			$("#sign").attr("action", "/signup");
+			
+			$scope.signUpButton = function () {
+				$scope.login = !$scope.login;
+			};
 
-			// var b = document.querySelector("#sign"); 
-
-			// b.setAttribute("action", "/signup");
+			if ($scope.login === true) {
+				$scope.sign = "Sign Up";
+				$("#sign").attr("action", "/signup");
+				$scope.account = "Already have an account?";
+				$scope.login = false;
+			} else if ($scope.login === false) {
+				$scope.sign = "Log In";
+				$("#sign").attr("action", "/login");
+				$scope.account = "Need an Account?";
+				$scope.login = true;
+			}
 		};
 	}]);
