93183112f9e994c503a68a014f1af71b7915877b
vikashiscool <vikash_parekh@yahoo.com>
2016-04-27 15:33:44 -0700

    Worked on main functionality, sliding, checklist, etc. Making it happen.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,13 +2,6 @@
 * public/scripts/app.js
 */
 
-// $(document).ready(function() {
-//     $('.step').click(function(){
-//         $(this).css('background', 'red');
-//     console.log("document.ready to rock");
-//     })
-// });
-
 
 var moneta = angular.module('moneta', ['ui.router'])
   .controller('mainController', function ($scope, $http) {
@@ -56,31 +49,18 @@ var moneta = angular.module('moneta', ['ui.router'])
 
 
 .controller('checklistController', function ($scope, $http){
-
-function checklist($scope) {
-    console.log("checlist function whaat");
+    console.log("checklist function whaat");
     $scope.todos = [
-    { name: "Red", id: 0, isChecked: false },
-    { name: "Blue", id: 1, isChecked: false },
-    { name: "Green", id: 2, isChecked: false },
-    { name: "Yellow", id: 3, isChecked: false },
-    { name: "Orange", id: 4, isChecked: false },
-    { name: "Purple", id: 5, isChecked: false }
+    { step: "Step 1", name: "Choose an IRA", id: 0, alias:"#zero", isChecked: false },
+    { step: "Step 2", name: "Open the IRA", id: 1, alias:"#one", isChecked: false },
+    { step: "Step 3", name: "Transfer money into your IRA account", id: 2, isChecked: false },
+    { step: "Step 4", name: "Investment 101", id: 3, isChecked: false },
+    { step: "Step 5", name: "Stocks and Bonds", id: 4, isChecked: false },
+    { step: "Step 6", name: "ETF and MF", id: 5, isChecked: false },
+    { step: "Step 7", name: "Strategy", id: 6, isChecked: false },
+    { step: "Step 8", name: "Invest based on your strategy", id: 7, isChecked: false }
     ];
-  }
-});
-
-// .controller("checklist", function($scope){
-//   console.log("Loading checklist controller");
-//   $scope.class = "unchecked";
-//   $scope.changeClass = function() {
-//     if($scope.class === "unchecked") 
-//       $scope.class = "checked";
-//     else {
-//       $scope.class = "unchecked";
-//     } 
-//   }
-// });
+})
 
 
 // .directive('checklist', ChecklistDirective);
@@ -92,4 +72,60 @@ function checklist($scope) {
 //     };
 //     return directive;
 //   }
+
+
+//Slideable Angular Directive
+
+.directive('slideable', function () {
+  console.log("slideable directive running");
+    return {
+        restrict:'C',
+        compile: function (element, attr) {
+            // wrap tag
+            var contents = element.html();
+            element.html('<div class="slideable_content" style="margin:0 !important; padding:0 !important" >' + contents + '</div>');
+
+            return function postLink(scope, element, attrs) {
+                // default properties
+                attrs.duration = (!attrs.duration) ? '1s' : attrs.duration;
+                attrs.easing = (!attrs.easing) ? 'ease-in-out' : attrs.easing;
+                element.css({
+                    'overflow': 'hidden',
+                    'height': '0px',
+                    'transitionProperty': 'height',
+                    'transitionDuration': attrs.duration,
+                    'transitionTimingFunction': attrs.easing
+                });
+            };
+        }
+    };
+})
+
+.directive('slideToggle', function() {
+    console.log("slideToggle directive running");
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var target = document.querySelector(attrs.slideToggle);
+            attrs.expanded = false;
+            element.bind('click', function() {
+                var content = target.querySelector('.slideable_content');
+                if(!attrs.expanded) {
+                    content.style.border = '1px solid rgba(0,0,0,0)';
+                    var y = content.clientHeight;
+                    content.style.border = 0;
+                    target.style.height = y + 'px';
+                } else {
+                    target.style.height = '0px';
+                }
+                attrs.expanded = !attrs.expanded;
+            });
+        }
+    }
+});
+
+
+
+
+
 
