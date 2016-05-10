780dce2b072997b96473bb50671dece90997c829
mseegan <mseegan@gmail.com>
2016-05-10 15:32:30 -0700

    removed extra connection to DB

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGOLAB_URI ||
+// mongoose.connect( process.env.MONGOLAB_URI ||
                   process.env.MONGOHQ_URL ||
                   "mongodb://localhost/moneta");
 // mongoose.connect("mongodb://localhost/moneta");
