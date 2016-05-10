c53e62fb54e436423dd172766df1ced9e3660523
mseegan <mseegan@gmail.com>
2016-05-10 14:45:18 -0700

    removed redundancy from app/index.js that was run in models/index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var express = require('express'),
     require('./config/passport')(passport); // pass passport for configuration
 
 // configuration ===============================================================
-mongoose.createConnect(process.env.MONGOLAB_URI || 'mongodb://localhost/moneta'); // connect to our database
+// mongoose.createConnect(process.env.MONGOLAB_URI || 'mongodb://localhost/moneta'); // connect to our database
 
 // mongoose.createConnection('mongodb://localhost/moneta'); // connect to our database on your local system named moneta. You can name it whatever you like, and it will be created as soon as you save some data to it.
 
